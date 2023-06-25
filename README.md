b1699e6d038171ed319bee49554c089f12548d56
Taki <talyasri@uoregon.edu>
2023-06-25 07:53:21 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple NFT project and
+Advanced IPFS NFT Project where tokenURI can be one of three different dogs randomly selected
---
+++ metadata/sample_metadata.py
@@ -0,0 +1,6 @@
+metadata_template = {
+    "name": "",
+    "description": "",
+    "image": "",
+    "attributes": [{"trait_type": "dankness", "value": 100}],
+}
